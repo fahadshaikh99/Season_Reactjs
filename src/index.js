107ba638184b2b0ef4d6b6907573b8778455c4dc
Fahad Shaikh <56800738+fahadshaikh99@users.noreply.github.com>
2020-06-06 11:15:53 +0500

    Location and state updated

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = { lat: null}
+  }
+  render(){
 
     window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
+        (position) => {
+          this.setState({ lat: position.coords.latitude});
+          console.log(position)
+        },
         (err) => console.log(err) 
     );
 
     return (
-        <div> Hi There ! </div>
+        <div>Latitude : {this.state.lat} </div>
     );
 }
 
+}
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
